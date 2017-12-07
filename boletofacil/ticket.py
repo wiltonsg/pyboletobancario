e093002e08820f9db308884d297e0161b92fb99e
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2017-12-07 19:41:52 -0300

    Add property token

--- boletofacil/ticket.py
+++ boletofacil/ticket.py
@@ -1,4 +1,12 @@
+import requests
+
+
 class Ticket(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, token):
+        self.__url = 'https://www.boletobancario.com/boletofacil/integration/api/v1/'
+        self.__token = token
+
+    @property
+    def _token(self):
+        return self.__token
--- tests/tests.py
+++ tests/tests.py
@@ -7,10 +7,13 @@ from boletofacil import Ticket
 class TicketTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.ticket = Ticket()
+        self.ticket = Ticket('12345')
 
     def test_creation_ticket(self):
         self.assertTrue(isinstance(self.ticket, Ticket))
+
+    def test_token(self):
+        self.assertEqual(self.ticket._token, '12345')
 
 
 if __name__ == '__main__':
