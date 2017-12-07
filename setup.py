0f2f529ac6a1acdd75ed404cf4fb4a6783471039
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2017-12-07 15:09:01 -0300

    [Closes #3] Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+
+DESCRIPTION = "Emita Boletos de Graça com CNPJ ou CPF. Você só é cobrado se receber!"
+
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README.md').read()
+except:
+    pass
+
+
+setup(name='pyboletofacil',
+      version='0.0.1',
+      description='Emita Boletos de Graça com CNPJ ou CPF. Você só é cobrado se receber!',
+      url='https://github.com/hudsonbrendon/pyboletofacil',
+      author='Hudson Brendon',
+      author_email='contato.hudsonbrendon@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=['tests*']),
+      install_requires=[
+          'requests',
+      ],
+      zip_safe=False)
