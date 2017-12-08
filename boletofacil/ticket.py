793e690b7ffcd5f8e80319196b6971e4c585ecc4
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2017-12-07 22:40:34 -0300

    Added token in url

--- boletofacil/ticket.py
+++ boletofacil/ticket.py
@@ -5,8 +5,8 @@ import requests
 class Ticket(object):
 
     def __init__(self, token):
-        self.__url = 'https://www.boletobancario.com/boletofacil/integration/api/v1/'
         self.__token = token
+        self.__url = 'https://www.boletobancario.com/boletofacil/integration/api/v1/?token={}&'.format(self._token)
 
     @property
     def _token(self):
@@ -17,3 +17,16 @@ class Ticket(object):
             if value:
                 self.__url += '{}={}&'.format(param, value)
         return self.__url
+
+    # def issue_charge(self, description, payer_name, payer_cpf_cnpj,
+    #                  reference=None, amount, due_date=None,
+    #                  installments=None, max_overdue_days=None, fine=None,
+    #                  interest=None, discount_amount=None, discount_days=None,
+    #                  payer_email=None, payer_secondary_email=None, payer_phone=None,
+    #                  payer_birth_date=None, billing_address_street=None,
+    #                  billing_address_number=None, billing_address_complement=None,
+    #                  billing_address_neighborhood=None, billing_address_city=None,
+    #                  billing_address_state=None, billing_address_postcode=None,
+    #                  notify_payer=None, notification_url=None, response_type='json',
+    #                  fee_schema_token=None, split_recipient=None):
+    #     data = requests.post(url)
--- tests/tests.py
+++ tests/tests.py
@@ -17,23 +17,23 @@ class TicketTestCase(unittest.TestCase):
 
     def test_get_url(self):
         self.assertEqual(self.ticket._get_url(param1='value1'),
-                         'https://www.boletobancario.com/boletofacil/integration/api/v1/param1=value1&')
+                         'https://www.boletobancario.com/boletofacil/integration/api/v1/?token=12345&param1=value1&')
 
     def test_get_url_two_params(self):
         self.assertEqual(self.ticket._get_url(param1='value1', param2='value2'),
-                         'https://www.boletobancario.com/boletofacil/integration/api/v1/param1=value1&param2=value2&')
+                         'https://www.boletobancario.com/boletofacil/integration/api/v1/?token=12345&param1=value1&param2=value2&')
 
     def test_get_url_three_params(self):
         self.assertEqual(self.ticket._get_url(param1='value1', param2='value2', param3='value3'),
-                         'https://www.boletobancario.com/boletofacil/integration/api/v1/param1=value1&param2=value2&param3=value3&')
+                         'https://www.boletobancario.com/boletofacil/integration/api/v1/?token=12345&param1=value1&param2=value2&param3=value3&')
 
     def test_get_url_param_none(self):
         self.assertEqual(self.ticket._get_url(param1='value1', param=None),
-                         'https://www.boletobancario.com/boletofacil/integration/api/v1/param1=value1&')
+                         'https://www.boletobancario.com/boletofacil/integration/api/v1/?token=12345&param1=value1&')
 
     def test_get_url_params_none(self):
         self.assertEqual(self.ticket._get_url(param1='value1', param2='value2', param3=None),
-                         'https://www.boletobancario.com/boletofacil/integration/api/v1/param1=value1&param2=value2&')
+                         'https://www.boletobancario.com/boletofacil/integration/api/v1/?token=12345&param1=value1&param2=value2&')
 
 
 if __name__ == '__main__':
