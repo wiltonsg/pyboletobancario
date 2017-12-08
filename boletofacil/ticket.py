2dc502c0ea042961d91d07f72e121b10dd0d4a3a
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2017-12-07 22:43:54 -0300

    Added token in url

--- boletofacil/ticket.py
+++ boletofacil/ticket.py
@@ -17,16 +17,3 @@ class Ticket(object):
             if value:
                 self.__url += '{}={}&'.format(param, value)
         return self.__url
-
-    # def issue_charge(self, description, payer_name, payer_cpf_cnpj,
-    #                  reference=None, amount, due_date=None,
-    #                  installments=None, max_overdue_days=None, fine=None,
-    #                  interest=None, discount_amount=None, discount_days=None,
-    #                  payer_email=None, payer_secondary_email=None, payer_phone=None,
-    #                  payer_birth_date=None, billing_address_street=None,
-    #                  billing_address_number=None, billing_address_complement=None,
-    #                  billing_address_neighborhood=None, billing_address_city=None,
-    #                  billing_address_state=None, billing_address_postcode=None,
-    #                  notify_payer=None, notification_url=None, response_type='json',
-    #                  fee_schema_token=None, split_recipient=None):
-    #     data = requests.post(url)
