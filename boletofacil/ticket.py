71cf6a3fa1a1a2a255d3c08e44a620c6771fcba1
Hudson Brendon <contato.hudsonbrendon@gmail.com>
2017-12-09 03:44:51 -0300

    Added method issue_charge()

--- boletofacil/ticket.py
+++ boletofacil/ticket.py
@@ -6,14 +6,27 @@ class Ticket(object):
 
     def __init__(self, token):
         self.__token = token
-        self.__url = 'https://www.boletobancario.com/boletofacil/integration/api/v1/?token={}&'.format(self._token)
+        self.__url = 'https://sandbox.boletobancario.com/boletofacil/integration/api/v1/{}?token={}&'
 
     @property
     def _token(self):
         return self.__token
 
-    def _get_url(self, **kwargs):
+    def _get_url(self, resource, **kwargs):
+        url = self.__url.format(resource, self._token)
         for param, value in sorted(kwargs.items(), key=operator.itemgetter(0)):
             if value:
-                self.__url += '{}={}&'.format(param, value)
-        return self.__url
+                url += '{}={}&'.format(param, value)
+        return url
+
+    def issue_charge(self, description, amount, payerName, payerCpfCnpj, **kwargs):
+        try:
+            data = requests.post(self._get_url(resource='issue-charge',
+                                               description=description,
+                                               amount=amount,
+                                               payerName=payerName,
+                                               payerCpfCnpj=payerCpfCnpj,
+                                               **kwargs)).json()
+        except Exception as error:
+            raise error
+        return data
--- tests/tests.py
+++ tests/tests.py
@@ -16,24 +16,47 @@ class TicketTestCase(unittest.TestCase):
         self.assertEqual(self.ticket._token, '12345')
 
     def test_get_url(self):
-        self.assertEqual(self.ticket._get_url(param1='value1'),
-                         'https://www.boletobancario.com/boletofacil/integration/api/v1/?token=12345&param1=value1&')
+        self.assertEqual(self.ticket._get_url(resource='issue-charge', param1='value1'),
+                         'https://sandbox.boletobancario.com/boletofacil/integration/api/v1/issue-charge?'
+                         'token=12345&param1=value1&')
 
     def test_get_url_two_params(self):
-        self.assertEqual(self.ticket._get_url(param1='value1', param2='value2'),
-                         'https://www.boletobancario.com/boletofacil/integration/api/v1/?token=12345&param1=value1&param2=value2&')
+        self.assertEqual(self.ticket._get_url(resource='issue-charge', param1='value1', param2='value2'),
+                         'https://sandbox.boletobancario.com/boletofacil/integration/api/v1/issue-charge?'
+                         'token=12345&param1=value1&param2=value2&')
 
     def test_get_url_three_params(self):
-        self.assertEqual(self.ticket._get_url(param1='value1', param2='value2', param3='value3'),
-                         'https://www.boletobancario.com/boletofacil/integration/api/v1/?token=12345&param1=value1&param2=value2&param3=value3&')
+        self.assertEqual(self.ticket._get_url(resource='issue-charge', param1='value1',
+                                              param2='value2', param3='value3'),
+                         'https://sandbox.boletobancario.com/boletofacil/integration/api/v1/issue-charge?'
+                         'token=12345&param1=value1&param2=value2&param3=value3&')
 
     def test_get_url_param_none(self):
-        self.assertEqual(self.ticket._get_url(param1='value1', param=None),
-                         'https://www.boletobancario.com/boletofacil/integration/api/v1/?token=12345&param1=value1&')
+        self.assertEqual(self.ticket._get_url(resource='issue-charge', param1='value1', param=None),
+                         'https://sandbox.boletobancario.com/boletofacil/integration/api/v1/issue-charge?'
+                         'token=12345&param1=value1&')
 
     def test_get_url_params_none(self):
-        self.assertEqual(self.ticket._get_url(param1='value1', param2='value2', param3=None),
-                         'https://www.boletobancario.com/boletofacil/integration/api/v1/?token=12345&param1=value1&param2=value2&')
+        self.assertEqual(self.ticket._get_url(resource='issue-charge', param1='value1', param2='value2', param3=None),
+                         'https://sandbox.boletobancario.com/boletofacil/integration/api/v1/issue-charge?'
+                         'token=12345&param1=value1&param2=value2&')
+
+    @requests_mock.Mocker()
+    def test_issue_charge(self, request_mock):
+        url = 'https://sandbox.boletobancario.com/boletofacil/integration/api/v1/issue-charge?' \
+              'token=12345&amount=500&description=pedido12345&payNumber=Testandoboleto&' \
+              'payerCpfCnpj=10090997441&payerName=Hudson%20Brendon&'
+        response = {'success': True,
+                    'data': {'charges': [
+                                         {'link': 'https://sandbox.boletobancario.com/',
+                                          'code': 10069811, 'payNumber': 'BOLETO TESTE - Não é válido para pagamento',
+                                          'checkoutUrl': 'https://sandbox.boletobancario.com/',
+                                          'dueDate': '12/12/2017'}]}}
+        request_mock.post(url, json=response)
+        self.assertEqual(self.ticket.issue_charge(description='pedido12345',
+                                                  amount=500, payerName='Hudson Brendon',
+                                                  payerCpfCnpj=10090997441,
+                                                  payNumber='Testandoboleto'), response)
 
 
 if __name__ == '__main__':
